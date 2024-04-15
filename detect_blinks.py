06d50a98dd3733d82a02ba16315c8273a54e5771
Coroutine <coroutine@Coroutines-Air.domain.name>
2024-04-15 22:11:23 +0530

    eyes detect

--- detect_blinks.py
+++ detect_blinks.py
@@ -61,24 +61,38 @@ predictor = dlib.shape_predictor(args["shape_predictor"])
 
 # start the video stream thread
 print("[INFO] starting video stream thread...")
-vs = FileVideoStream(args["video"]).start()
-fileStream = True
+# vs = FileVideoStream(args["video"]).start()
+# fileStream = True
+
 # vs = VideoStream(src=0).start()
 # vs = VideoStream(usePiCamera=True).start()
 # fileStream = False
 time.sleep(1.0)
+# import cv2
 
-# loop over frames from the video stream
+# Open the default camera (usually the first camera connected)
+cap = cv2.VideoCapture(0)
+
+# Check if the camera opened successfully
+if not cap.isOpened():
+    print("Error: Unable to open camera.")
+    exit()
+
+# Loop to read and display frames from the camera
 while True:
+    # Capture frame-by-frame
+	ret, frame = cap.read()
+# loop over frames from the video stream
+# while True:
 	# if this is a file video stream, then we need to check if
 	# there any more frames left in the buffer to process
-	if fileStream and not vs.more():
-		break
+	# if fileStream and not vs.more():
+	# 	break
 
 	# grab the frame from the threaded video file stream, resize
 	# it, and convert it to grayscale
 	# channels)
-	frame = vs.read()
+	# frame = vs.read()
 	frame = imutils.resize(frame, width=450)
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
